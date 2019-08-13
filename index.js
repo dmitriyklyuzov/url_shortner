d215b2a3c408f7199daa273bb638eebd1596eff6
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2019-08-12 23:44:48 -0400

    App - connect to Db from index

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDb = require('./config/db');
 
 // initialize app
 const app = express();
+
+// connect to database
+connectDb();
 
 // accept json data into api
 app.use(express.json({ extended: false }));
